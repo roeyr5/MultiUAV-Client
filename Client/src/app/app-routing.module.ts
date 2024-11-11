896ac978016e4b74644f9275340222c5fd8079c2
roeyr5 <roey9191911@gmail.com>
2024-11-11 18:55:52 +0200

    first

---
+++ Client/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+
+const routes: Routes = [
+  { path: 'signin', component: SigninComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
