ad225804f8f15d4ae95709f50f990518ac17587d
roeyr5 <157245790+roeyr5@users.noreply.github.com>
2024-11-28 15:22:58 +0200

    Update main.component.ts

--- Client/src/app/components/main/main.component.ts
+++ Client/src/app/components/main/main.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { SignalRService } from 'src/app/services/signalr.service';
+import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  
+  public receivedmessage:string = '';
+  protected parametersarray:string[]=[];
+  
+  constructor(private signalRService : SignalRService , private userservice:UserService){}
 
+  public ngOnInit(): void {
+      this.signalRService.startConnection().subscribe((response)=>{
+        console.log("worked signalR");
+        this.signalRService.receiveMessage().subscribe((message)=>{
+          this.receivedmessage=message;
+        });
+      });
+
+      this.GetParameters();
+  }
+
+  protected sendMessage(message: string): void {
+    this.signalRService.sendMessage(message);
+  }
+
+  protected GetParameters(){    
+    this.userservice.list().subscribe((res)=>{
+      this.parametersarray=res;
+    },(err) =>{
+      console.error("error" , err);
+    }
+    )
+  }
 }
