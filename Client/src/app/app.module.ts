07bdb094e32fc262bcb8f0079b3256306019fb8b
Reinman <roey9191911@gmail.com>
2024-12-04 18:17:13 +0200

    new

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { SigninComponent } from './components/signin/signin.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './components/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
+
 
 @NgModule({
   declarations: [
@@ -14,10 +19,13 @@ import { MainComponent } from './components/main/main.component';
     MainComponent,
   ],
   imports: [
+    MatInputModule,
+    MatButtonModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
 ],
   providers: [],
   bootstrap: [AppComponent]
